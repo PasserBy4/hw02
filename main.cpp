ae3f81e769bcb9569f1a99508531ed79880735bc
高逸飞 <passerby@gaoyifeideMacBook-Air.local>
2023-07-14 18:53:49 +0800

    finish hw02

--- main.cpp
+++ main.cpp
@@ -1,35 +1,31 @@
 /* 基于智能指针实现双向链表 */
 #include <cstdio>
+#include <exception>
 #include <memory>
+#include <stdexcept>
+#include <utility>
+#include <type_traits>
+#include <iterator>
+#include <cstddef>
+#include <iostream>
 
+template <typename T>
 struct Node {
     // 这两个指针会造成什么问题？请修复
-    std::shared_ptr<Node> next;
-    std::shared_ptr<Node> prev;
+    std::unique_ptr<Node> next;
+    Node* prev;
     // 如果能改成 unique_ptr 就更好了!
 
-    int value;
+    T value;
 
     // 这个构造函数有什么可以改进的？
-    Node(int val) {
-        value = val;
-    }
-
-    void insert(int val) {
-        auto node = std::make_shared<Node>(val);
-        node->next = next;
-        node->prev = prev;
-        if (prev)
-            prev->next = node;
-        if (next)
-            next->prev = node;
-    }
+    explicit Node(int val): next{nullptr}, prev{nullptr}, value{val} {}
 
     void erase() {
-        if (prev)
-            prev->next = next;
         if (next)
             next->prev = prev;
+        if (prev)
+            prev->next = std::move(next);
     }
 
     ~Node() {
@@ -37,59 +33,115 @@ struct Node {
     }
 };
 
+template<typename T>
 struct List {
-    std::shared_ptr<Node> head;
 
+    class iterator {
+    private:
+        Node<T>* m_ptr;
+    public:
+        using iterator_category = std::bidirectional_iterator_tag;
+        using diffrent_type     = std::ptrdiff_t;
+        using value_type        = Node<T>;
+        using pointer           = value_type*;
+        using reference         = value_type&; 
+
+        iterator(pointer ptr): m_ptr(ptr) {}
+        reference operator*() { return *m_ptr; }
+        pointer operator->() { return m_ptr; }
+
+        iterator& operator++(){
+            m_ptr = m_ptr -> next.get();
+            return *this;
+        }
+        
+        iterator operator++(int){
+            iterator tmp = *this;
+            ++(*this);
+            return tmp;
+        }
+
+        friend bool operator == (const iterator& a, const iterator& b){
+            return a.m_ptr == b.m_ptr;
+        }
+        friend bool operator != (const iterator& a, const iterator& b){
+            return a.m_ptr != b.m_ptr;
+        }
+    };
+    std::unique_ptr<Node<T> > head;
+    Node<T>* back{nullptr};
     List() = default;
 
     List(List const &other) {
         printf("List 被拷贝！\n");
-        head = other.head;  // 这是浅拷贝！
         // 请实现拷贝构造函数为 **深拷贝**
+        for(auto it = other.begin(); it != other.end(); it++){
+            push_back(it->value);
+        }
     }
-
-    List &operator=(List const &) = delete;  // 为什么删除拷贝赋值函数也不出错？
 
     List(List &&) = default;
     List &operator=(List &&) = default;
 
-    Node *front() const {
+    Node<T> *front() const {
         return head.get();
     }
 
-    int pop_front() {
-        int ret = head->value;
-        head = head->next;
+    T pop_front() {
+        if(begin() == end()){
+            throw std::out_of_range("pop_front()");
+        }
+        T ret = head->value;
+        if(head.get() == back) 
+            back = nullptr;
+        head = std::move(head->next);
         return ret;
     }
 
-    void push_front(int value) {
-        auto node = std::make_shared<Node>(value);
-        node->next = head;
+    void push_front(const T& value) {
+        auto node = std::make_unique<Node<T>>(value);
         if (head)
-            head->prev = node;
-        head = node;
+            head->prev = node.get();
+        else
+            back = node.get();
+        node -> next = std::move(head);
+        head = std::move(node);
     }
 
-    Node *at(size_t index) const {
+    Node<T> *at(size_t index) const {
         auto curr = front();
         for (size_t i = 0; i < index; i++) {
             curr = curr->next.get();
         }
         return curr;
     }
+
+    void push_back(const T& value){
+        auto node = std::make_unique<Node<T>>(value);
+        if(back != nullptr){
+            node -> prev = back;
+            back -> next = std::move(node);
+            back = back->next.get();
+        }
+        else{
+            head = std::move(node);
+            back = head.get();
+        }
+    }
+    iterator begin() const {return iterator(head.get());}
+    iterator end() const {return iterator(nullptr);}
 };
 
-void print(List lst) {  // 有什么值得改进的？
+void print(const List<int>& lst) {  // 有什么值得改进的？
     printf("[");
-    for (auto curr = lst.front(); curr; curr = curr->next.get()) {
-        printf(" %d", curr->value);
+    for (auto &v: lst) {
+        printf(" %d", v.value);
     }
-    printf(" ]\n");
+    printf(" ]\n"); 
 }
 
 int main() {
-    List a;
+    List<int> a;
 
     a.push_front(7);
     a.push_front(5);
@@ -105,7 +157,7 @@ int main() {
 
     print(a);   // [ 1 4 2 8 5 7 ]
 
-    List b = a;
+    List<int> b = a;
 
     a.at(3)->erase();
 
